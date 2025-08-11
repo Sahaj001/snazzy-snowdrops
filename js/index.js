73453ddd3ff85c7e071861f3cfe931179ecf0e08
base-13 <86418442+base-13@users.noreply.github.com>
2025-08-12 01:27:21 +0530

    remove some useless code and fix error in loading json

--- js/index.js
+++ js/index.js
@@ -1,33 +1,31 @@
 async function loadProject(pyodide) {
-
     const response = await fetch('../dist/src.zip'); // served from dist
     let buffer = await response.arrayBuffer();
 
-    await pyodide.unpackArchive(buffer, "zip"); // by default, unpacks to the current dir
+    await pyodide.unpackArchive(buffer, 'zip'); // by default, unpacks to the current dir
 
-    console.log("Project loaded...");
+    console.log('Project loaded...');
 }
 
 async function main() {
-    console.log("Loading Pyodide...");
+    console.log('Loading Pyodide...');
     const pyodide = await loadPyodide();
-    pyodide.setDebug(true)
+    pyodide.setDebug(true);
 
-    console.log("Loading python project...")
+    console.log('Loading python project...');
     await loadProject(pyodide);
 
-    console.log("Loading Python packages...");
+    console.log('Loading Python packages...');
     await pyodide.runPythonAsync(`
         import sys
+        sys.path.append('/src')
         import main
+        await main.start()
     `);
 
-
     // Make the canvas available to Python
-    const canvas = document.getElementById("gameCanvas");
-    pyodide.globals.set("canvas", canvas);
-
-    await pyodide.runPythonAsync("import sys; sys.path.append('/src'); import main");
+    const canvas = document.getElementById('gameCanvas');
+    pyodide.globals.set('canvas', canvas);
 }
 
 main();
