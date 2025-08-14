cbdece0da37de62d050d3e38789a6d9dda1b7fe8
base-13 <86418442+base-13@users.noreply.github.com>
2025-08-15 01:57:20 +0530

    fix: set self._initialized = True instead of False

--- src/menu/pause_menu.py
+++ src/menu/pause_menu.py
@@ -24,7 +24,7 @@ class PauseMenu:
             self._main_menu_btn.onclick = lambda _: self._main_menu_btn_onclick()
             self._settings_btn.onclick = lambda _: self._settings_btn_onclick()
 
-            self._initialized = False
+            self._initialized = True
 
     def __new__(cls) -> Self:
         if cls._instance is None:
